6738034edcc7c2f3d7c8f5503a73e86c90bf434b
jankar-with-tarek <avirus170@gmail.com>
2022-09-18 22:42:13 +0600

    updated error

--- src/pages/SendContact.js
+++ src/pages/SendContact.js
@@ -20,18 +20,17 @@ const SendContact = () => {
                 {error && <p className='text-red-500 text-sm mb-3'>{error}</p>}
                 <div>
                     <input className='mr-2' onClick={() => setChecked(!checked)} type="checkbox" name="check" id="" />
-                    <label className='text-sm' htmlFor="check">
+                    <label className='text-sm mb-16' htmlFor="check">
                         * [Sample legal text] The personal data you have provided will be processed by XXXXX for purposes of providing you the service. The legal basis of the processing is XXXXX. Your data will not be transferred nor assigned to third parties. You can exercise your right to access, rectify and delete your data, as well as the other rights granted by law by sending an email to XXXXX. For further information, please check our privacy policy XXXXX.</label>
                 </div>
 
                 {checked ?
-                    <button className='btn-secondary w-full py-3 mt-12' type='submit'>
-                        <a target='_blank' rel="noreferrer" href="https://github.com/omarrfaruk/Interactly-task">
-                            <span>Send your answer</span>
-                            <span><HiOutlineArrowNarrowRight className='inline ml-10' /></span>
-                        </a>
-                    </button> :
-                    <button onClick={() => setError('To continue you must accept the terms & conditions.')} className='btn-secondary w-full py-3 mt-12' type='submit'>
+                    <a className='btn-secondary w-full block mt-10 py-3' target='_blank' rel="noopener noreferrer" href="https://github.com/omarrfaruk">
+                        <span>Send your answer</span>
+                        <span><HiOutlineArrowNarrowRight className='inline ml-10' /></span>
+                    </a>
+                    :
+                    <button onClick={() => setError('To continue you must accept the terms & conditions.')} className='btn-secondary w-full py-3 mt-10' type='submit'>
                         <span>Send your answer</span>
                         <span><HiOutlineArrowNarrowRight className='inline ml-10' /></span>
                     </button>
